406f8db1e10105d435cff39c05c2a06456db2636
Daniel Castillo <daniel.castillosecilla@fujitsu.com>
2021-12-15 16:14:34 +0100

    restoring

--- src/main/java/com/drugsapp/repositories/DrugSetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.drugsapp.repositories;
-
-import com.drugsapp.entities.DrugSet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DrugSetRepository extends JpaRepository<DrugSet, UUID> {
-}
--- src/main/java/com/drugsapp/repositories/DrugUpdateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.drugsapp.repositories;
-
-import com.drugsapp.entities.DrugUpdate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DrugUpdateRepository extends JpaRepository<DrugUpdate, UUID> {
-}
