80b26af3c59bb116f77bf807f5267760adbe9c3e
Daniel Castillo-Secilla <daniel.castillosecilla@fujitsu.com>
2022-02-21 14:01:38 +0100

    Spring batch integration in progress

--- application/src/main/java/com/fujitsu/drugsapp/DrugsAppApplication.java
+++ application/src/main/java/com/fujitsu/drugsapp/DrugsAppApplication.java
@@ -1,9 +1,17 @@
 package com.fujitsu.drugsapp;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.fujitsu"})
+@EnableJpaRepositories("com.fujitsu")
+@EntityScan("com.fujitsu")
+@EnableBatchProcessing
+@EnableScheduling
 public class DrugsAppApplication {
 
 	public static void main(String[] args) {
